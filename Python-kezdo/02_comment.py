37ea4bebc757d54825b4ce9e232d6501ea526a66
djmorphy <djmorphy@gmail.com>
2023-03-12 17:48:13 +0100

    function practice

---
+++ Python-kezdo/02_comment.py
@@ -0,0 +1,10 @@
+#egysoros comment
+print("# -el írok egy soros kommentet")
+
+""" 
+ez a három darab idézőjel a többsoros komment
+"""
+
+'ez is többsoros komment'
+
+print(" több soros komment az három db duplaidézőjel vagy egy darab szimpla idézőjel nyitó és zárótagel ")
---
+++ Python-kezdo/04_tipusok.py
@@ -0,0 +1,35 @@
+
+
+"""
+Numbers
+String
+Boolean
+List
+Tuple
+Dictionary
+User defined
+
+"""
+szam1 = 10
+szam2 = 24.3243242
+szam3 = 32.3e17 #ez number stcientificted azaz tudományos
+szam4 = .876j  #ez is number ez egy komplex szám
+
+igaz = True
+hamis = False
+#string
+nev1 = "djmorphy"
+hajszin = 'nincs'
+
+#List
+lista = []
+
+#Tuple
+my_tuple = ()
+
+#Dictionary
+my_dict = {}
+
+
+#szam1 típus kiirítésa
+print(type(szam1))
